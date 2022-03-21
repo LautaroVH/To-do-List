3c6768969e91184794572d7c3c29cdfb0f4251c1
LautaroVh <lautarovh@outlook.com>
2022-03-21 17:57:16 -0300

    se agrega el proyecto

--- index.js
+++ index.js
@@ -29,11 +29,11 @@ app.use(express.static("public"));
 //agregar Router
 app.use("/", router);
 
-const host = process.env.HOST || "0.0.0.0";
+// const host = process.env.HOST || "0.0.0.0";
 const port = process.env.PORT || 3000;
 
 
-app.listen(port, host, () => {
+app.listen(port,  () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
 
