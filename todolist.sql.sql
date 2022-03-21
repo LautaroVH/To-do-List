42facb4c1160733bd011869b5885c5e1a49c28d2
LautaroVh <lautarovh@outlook.com>
2022-03-21 00:15:49 -0300

    solucionar package.json

---
+++ todolist.sql.sql
@@ -0,0 +1,101 @@
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: todo-list1
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alma`
+--
+
+DROP TABLE IF EXISTS `alma`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alma` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lista` varchar(200) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alma`
+--
+
+LOCK TABLES `alma` WRITE;
+/*!40000 ALTER TABLE `alma` DISABLE KEYS */;
+INSERT INTO `alma` VALUES (1,'Compras','2022-03-29'),(2,'Vacaciones','2022-03-31');
+/*!40000 ALTER TABLE `alma` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `almas`
+--
+
+DROP TABLE IF EXISTS `almas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `almas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lista` varchar(200) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `almas`
+--
+
+LOCK TABLES `almas` WRITE;
+/*!40000 ALTER TABLE `almas` DISABLE KEYS */;
+INSERT INTO `almas` VALUES (1,'Shopping','2022-03-21'),(2,'Holidays','2022-03-28'),(5,'Sports','2022-03-29'),(42,'','0000-00-00'),(43,'','0000-00-00'),(44,'','0000-00-00'),(45,'','0000-00-00');
+/*!40000 ALTER TABLE `almas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `listas`
+--
+
+DROP TABLE IF EXISTS `listas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `listas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ListName` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `listas`
+--
+
+LOCK TABLES `listas` WRITE;
+/*!40000 ALTER TABLE `listas` DISABLE KEYS */;
+INSERT INTO `listas` VALUES (2,''),(3,''),(4,''),(5,''),(6,''),(7,''),(8,''),(9,''),(10,''),(11,''),(12,''),(13,''),(14,''),(16,''),(18,''),(19,''),(20,''),(21,''),(22,''),(23,''),(24,''),(25,''),(26,''),(27,''),(28,''),(29,''),(30,''),(31,''),(32,''),(33,''),(34,''),(35,''),(36,''),(37,''),(38,''),(39,''),(40,''),(41,'');
+/*!40000 ALTER TABLE `listas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-20 23:46:30
